562a939fb624e28ed839c3c41eb2a9ebdc193740
Ayaulym <altynbek_ayaulym0@mail.ru>
2026-01-25 14:12:38 +0500

    Add 1.py and README.txt

---
+++ 1.py
@@ -0,0 +1,48 @@
+#This is comment
+print("Hello Wolrd") #This is comment
+print("Python is fun")
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+print("Hello"); print("How are you?"); print("Bye bye!")
+print("This will work!")
+print('This will also work')
+print("This will work!")
+print('This will also work!')
+print(3)
+print(358)
+print(50000)
+print(3+3)
+print(2*5)
+print("I'm", 35, "years old")
+#print("Hwllo World")
+print('cheers, mate')
+#This is a comment
+#written in
+#more than just one line
+print('cheers, mate')
+"""This is a comment
+written in
+more than just one line
+"""
+print('cheers, mate')
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4#x is of type int
+x = "Sally" #x is now of type str
+print(x)
+x = str(3)#x will be '3'
+y = int(3)#y will be 3
+z = float(3)#z will be 3.0
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+x = "John"
+# is the same as
+x = 'John'
+a = 4
+A = "Sally"
+#A will not overwrite a
